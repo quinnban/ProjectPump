c8166dc76ff7bdf2c7de06fb4a6f680b00f030f8
Quinn <quinn.bancescu@gmail.com>
2022-10-03 15:14:36 -0600

    update pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
