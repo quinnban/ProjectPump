12db02fc04381a116c0ef265170addca7fd4e161
Quinn <quinn.bancescu@gmail.com>
2022-10-03 15:34:48 -0600

    added module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_PIPE } from '@nestjs/core';
+import { UserService } from './Users/user.service';
+import { UserModule } from './Users/user.module';
 
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService,
     {
